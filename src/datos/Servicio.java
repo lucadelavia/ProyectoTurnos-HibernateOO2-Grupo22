2ed5b8ab813775ae9fb1d6382925ee3c9f9a9dd8
luca <lucadelavia@gmail.com>
2025-05-04 02:21:27 -0300

    Se agregan mapeos y base de datos configurada.

--- src/datos/Servicio.java
+++ src/datos/Servicio.java
@@ -1,5 +1,59 @@
 package datos;
 
-public class Servicio {
+import java.util.Objects;
 
+public class Servicio {
+	
+	private int id;
+	private String nombreServicio;
+	private int duracion;
+	
+	public Servicio(String nombreServicio, int duracion) {
+		super();
+		this.nombreServicio = nombreServicio;
+		this.duracion = duracion;
+	}
+
+	public Servicio() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Servicio [id=" + id + ", nombreServicio=" + nombreServicio + ", duracion=" + duracion + "]";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Servicio other = (Servicio) obj;
+		return duracion == other.duracion && id == other.id && Objects.equals(nombreServicio, other.nombreServicio);
+	}
+	
+	public int getId() {
+		return id;
+	}
+	protected void setId(int id) {
+		this.id = id;
+	}
+	public String getNombreServicio() {
+		return nombreServicio;
+	}
+	public void setNombreServicio(String nombreServicio) {
+		this.nombreServicio = nombreServicio;
+	}
+	public int getDuracion() {
+		return duracion;
+	}
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+	
+	
+	
 }
