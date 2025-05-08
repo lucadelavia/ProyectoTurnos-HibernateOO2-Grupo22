2dbcbe723e8a9d70054902b9bfd2165a14a9b4a8
Sebastian Ariel Sdrubolini <sevsdrubolini@gmail.com>
2025-05-08 00:19:51 -0300

    feat: dao de cliente, empleado y usuario

---
+++ src/dao/UsuarioDao.java
@@ -0,0 +1,75 @@
+package dao;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import datos.Usuario;
+
+public class UsuarioDao {
+	private static Session session;
+    private Transaction tx;
+
+    private void iniciaOperacion() throws HibernateException {
+        session = HibernateUtil.getSessionFactory().openSession();
+        tx = session.beginTransaction();
+    }
+	
+	private void manejaExcepcion(HibernateException he) throws HibernateException {
+        tx.rollback();
+        throw new HibernateException("ERROR en la capa de acceso a datos", he);
+    }
+	
+	public int agregar(Usuario objeto) {
+        int id = 0;
+        try {
+            iniciaOperacion();
+            id = Integer.parseInt(session.save(objeto).toString());
+            tx.commit();
+        } catch (HibernateException he) {
+            manejaExcepcion(he);
+            throw he; 
+        } finally {
+            session.close();
+        }
+        return id;
+    }
+    
+    public Usuario traer(long idUsuario) {
+    	Usuario objeto = null;
+		try {
+			iniciaOperacion();
+			objeto = (Usuario) session.get(Usuario.class, idUsuario);
+		} finally {
+			session.close();
+		}
+		return objeto;
+	}
+
+	public void actualizar(Usuario objeto) {
+		try {
+			iniciaOperacion();
+			session.update(objeto);
+			tx.commit();
+		} catch (HibernateException he) {
+			manejaExcepcion(he);
+			throw he;
+		} finally {
+			session.close();
+		}
+	}
+
+	public void eliminar(Usuario objeto) {
+		try {
+			iniciaOperacion();
+			session.delete(objeto);
+			tx.commit();
+		} catch (HibernateException he) {
+			manejaExcepcion(he);
+			throw he;
+		} finally {
+			session.close();
+		}
+	}
+	
+}
