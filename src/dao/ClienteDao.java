e593f00685d1e727ce2983a1f31fc76390dec05e
ValentinaMohamed <mohamedvalentinab@gmail.com>
2025-05-08 00:11:50 -0300

    dao de cliente

---
+++ src/dao/ClienteDao.java
@@ -0,0 +1,71 @@
+package dao;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import datos.Cliente;
+
+public class ClienteDao {
+    private static Session session;
+    private Transaction tx;
+
+    private void iniciaOperacion() throws HibernateException {
+        session = HibernateUtil.getSessionFactory().openSession();
+        tx = session.beginTransaction();
+    }
+
+    private void manejaExcepcion(HibernateException he) throws HibernateException {
+        tx.rollback();
+        throw new HibernateException("ERROR en la capa de acceso a datos", he);
+    }
+
+    public int agregar(Cliente objeto) {
+        int id = 0;
+        try {
+            iniciaOperacion();
+            id = Integer.parseInt(session.save(objeto).toString());
+            tx.commit();
+        } catch (HibernateException he) {
+            manejaExcepcion(he);
+        } finally {
+            session.close();
+        }
+        return id;
+    }
+
+    public void actualizar(Cliente objeto) {
+        try {
+            iniciaOperacion();
+            session.update(objeto);
+            tx.commit();
+        } catch (HibernateException he) {
+            manejaExcepcion(he);
+        } finally {
+            session.close();
+        }
+    }
+
+    public void eliminar(Cliente objeto) {
+        try {
+            iniciaOperacion();
+            session.delete(objeto);
+            tx.commit();
+        } catch (HibernateException he) {
+            manejaExcepcion(he);
+        } finally {
+            session.close();
+        }
+    }
+
+    public Cliente traer(int idCliente) {
+        Cliente objeto = null;
+        try {
+            iniciaOperacion();
+            objeto = (Cliente) session.get(Cliente.class, idCliente);
+        } finally {
+            session.close();
+        }
+        return objeto;
+    }
+}
